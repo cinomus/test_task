66c17af429a5c172a1611f378c330d464b9b7326
cinomus <57807571+cinomus@users.noreply.github.com>
2023-05-17 09:56:57 +0300

    Update game.py

--- game.py
+++ game.py
@@ -1,5 +1,5 @@
 from copy import deepcopy
-
+from typing import List
 import psycopg
 
 
@@ -9,10 +9,21 @@ class GameOfLife:
 
     @classmethod
     async def init_board(cls):
+        '''
+        Создает и возвращает двумерный массив поля.
+        Args: None
+        Returns: List[List[int]]
+        '''
         return [[0 for _ in range(cls.width)] for _ in range(cls.height)]
 
     @classmethod
-    async def start_game(cls, field):
+    async def start_game(cls, field: List[List[int]]):
+        '''
+        Создает игру и возвращает ее id
+        Args: 
+            field: List[List[int]]
+        Returns: int
+        '''
         async with await psycopg.AsyncConnection.connect('postgresql://admin:admin@localhost:5432/postgres') as conn:
             async with conn.cursor() as cur:
                 await cur.execute("INSERT INTO Games (status) VALUES (%s) RETURNING id;", ('in_process',))
@@ -22,21 +33,40 @@ class GameOfLife:
                 return game_id
 
     @classmethod
-    async def get_game(cls, game_id):
+    async def get_game(cls, game_id: int):
+        '''
+        Получение данных игры по id.
+        Args: 
+            game_id: int
+        Returns: List
+        '''
         async with await psycopg.AsyncConnection.connect('postgresql://admin:admin@localhost:5432/postgres') as conn:
             async with conn.cursor() as cur:
                 await cur.execute('SELECT id, status FROM Games WHERE id=%s ', (game_id,))
                 return await cur.fetchone()
 
     @classmethod
-    async def get_game_step(cls, game_id, step_number):
+    async def get_game_step(cls, game_id: int, step_number: int):
+        '''
+        Получение шага игры по id
+        Args:  
+            game_id: int, 
+            step_number: int
+        Returns: List
+        '''
         async with await psycopg.AsyncConnection.connect('postgresql://admin:admin@localhost:5432/postgres') as conn:
             async with conn.cursor() as cur:
                 await cur.execute('SELECT field FROM GameSteps WHERE game_id=%s AND number=%s', (game_id, step_number))
                 return await cur.fetchone()
 
     @classmethod
-    async def next_step(cls, game_id):
+    async def next_step(cls, game_id: int):
+        '''
+        Получение следующего шага игры
+        Args: 
+            game_id: int
+        Returns: List
+        '''
         async with await psycopg.AsyncConnection.connect('postgresql://admin:admin@localhost:5432/postgres') as conn:
             async with conn.cursor() as cur:
                 await cur.execute(
